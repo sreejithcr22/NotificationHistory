0745fabd8528428f6bcebce833e872407a374333
sreejith <sreejithcr2@gmail.com>
2024-05-15 20:23:36 +0530

    Release v1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.sreejith.notificationhistory"
         minSdk = 30
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
