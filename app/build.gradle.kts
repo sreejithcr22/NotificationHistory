d52e0ef404bc0d809b9cca121e2d20d7dfcadd45
sreejith <sreejithcr2@gmail.com>
2024-05-15 11:20:00 +0530

    Updated app icon and app name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.sreejith.notificationhistory"
         minSdk = 30
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
